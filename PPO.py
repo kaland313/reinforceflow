bc48554f1c559f725e3b0ddcc72e3547a7826c22
kalap <kalapos.andras@gmail.com>
2020-02-12 11:05:10 +0100

    Generalized Advantage Estimate implemented and tested (but convergence broken on LunarLanderCont.)

---
+++ PPO.py
@@ -0,0 +1,17 @@
+
+
+from ActorCritic import ActorCritic
+
+
+class PPO(ActorCritic):
+    def __init__(self,
+                 env,
+                 actor_learning_rate=1e-3,
+                 critic_learning_rate=1e-2,
+                 discount_gamma=0.99,
+                 generalized_advantage_estimate_lambda=0.97):
+        super(PPO, self).__init__(env, actor_learning_rate, critic_learning_rate, discount_gamma,
+                                  generalized_advantage_estimate_lambda)
+
+
+
--- tests/test_policygradient.py
+++ tests/test_policygradient.py
@@ -1,28 +1,47 @@
 import unittest
 from unittest import TestCase
 import numpy as np
-from utils.reward_calc import calculate_discounted_returns
+from utils.reward_calc import calculate_discounted_returns, calculate_generalized_advantage_estimate
 
 
-# Credits: https://github.com/csxeba/Learn-TensorFlow-2.0-The-Hard-Way
-def discount_rewards(rewards, gamma=0.99):
-    discounted_r = np.zeros_like(rewards)
-    running_add = 0
-    for t in reversed(range(0, len(rewards))):
-        running_add = running_add * gamma + rewards[t]
-        discounted_r[t] = running_add
-    return discounted_r
+# Credits: https://github.com/csxeba/trickster/blob/dev/trickster/processing/reward_shaping.py
+def discount(rewards, dones, gamma=None):
+    discounted = np.empty_like(rewards)
+    cumulative_sum = 0.
+    for i in range(len(rewards) - 1, -1, -1):
+        cumulative_sum *= (1 - dones[i]) * gamma
+        cumulative_sum += rewards[i]
+        discounted[i] = cumulative_sum
+    return discounted
+
+def compute_gae(rewards, values, values_next, dones, gamma, lmbda):
+    delta = rewards + gamma * values_next * (1 - dones) - values
+    advantages = discount(delta, dones, gamma=gamma * lmbda)
+    returns = advantages + values
+    return advantages, returns
 
 
 class Test(TestCase):
     def test_calculate_discounted_returns(self):
         rewards = np.random.random(10)
-        np.testing.assert_allclose(discount_rewards(rewards, gamma=0.99),
+        dones = np.zeros_like(rewards)
+        dones[-1] = 1
+        np.testing.assert_allclose(discount(rewards, dones, gamma=0.99),
                                    calculate_discounted_returns(rewards, gamma=0.99))
-        rewards = np.random.random(10000)*10000
-        np.testing.assert_allclose(discount_rewards(rewards, gamma=0.99),
-                                   calculate_discounted_returns(rewards, gamma=0.99))
+        # rewards = np.random.random(10000)*10000
+        # np.testing.assert_allclose(discount(rewards, dones, gamma=0.99),
+        #                            calculate_discounted_returns(rewards, gamma=0.99))
 
+    def test_calculate_gae(self):
+        gamma = 0.99
+        gae_lambda = 0.97
+        size = 10
+        rewards = np.random.random(size)
+        values = np.random.random(size + 1)
+        dones = np.zeros_like(rewards)
+        dones[-1] = 1
+        np.testing.assert_allclose(compute_gae(rewards, values[0:-1], values[1:], dones, gamma, gae_lambda),
+                                   calculate_generalized_advantage_estimate(rewards, values, dones, gae_lambda, gamma))
 
 if __name__ == '__main__':
     unittest.main()

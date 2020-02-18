Fix neg_log_prob_a_t sign in PPO loss calculation.

agents/PPO.py
@@ -11,12 +11,14 @@ class PPO(ActorCritic):
                  critic_learning_rate=1e-2,
                  discount_gamma=0.99,
                  generalized_advantage_estimate_lambda=0.97,
-                 global_std_for_continuous_policy=False,
+                 global_std_for_gaussian_policy=False,
+                 tanh_transform_gaussian_policy=True,
                  algo_str="PPO",
                  clip_epsilon=0.2,
                  policy_train_epochs=10):
         super(PPO, self).__init__(env, actor_learning_rate, critic_learning_rate, discount_gamma,
-                                  generalized_advantage_estimate_lambda, global_std_for_continuous_policy, algo_str)
+                                  generalized_advantage_estimate_lambda, global_std_for_gaussian_policy,
+                                  tanh_transform_gaussian_policy, algo_str)
         self.clip_epsilon = clip_epsilon
         self.policy_train_epochs = policy_train_epochs
 
@@ -51,16 +53,18 @@ class PPO(ActorCritic):
         # According to https://arxiv.org/abs/1707.06347: the probability ratioris clipped at 1−epsion or 1 + epsilon
         # depending on whether the advantage is positive or negative"
         # pos_advantage = tf.cast(normalized_advantages > 0, dtype='float32')
-        # active_clip_limit = (1. + self.clip_epsilon) * pos_advantage + (1. - self.clip_epsilon) * (1. - pos_advantage)
+        # active_clip_limit = (1. + self.clip_epsilon) * pos_advantage + (1. - self.clip_epsilon) * (
+        #         1. - pos_advantage)
         # clipped_term = active_clip_limit * advantage_estimate
 
         with tf.GradientTape() as tape:
             network_output = self.actor_model(observations)
             neg_log_prob_at = self.proba_distribution.neg_log_prob_a_t(network_output, actions)
-            prob_ratio = tf.exp(neg_log_prob_at - old_neg_log_prob_a_t)
+            # Both neg_log_prob_a_ts must be multiplied by -1 because prob_ratio = exp(log_prob_a_t - old_log_prob_a_t)
+            prob_ratio = tf.exp(-neg_log_prob_at + old_neg_log_prob_a_t)
             L_CPI = prob_ratio * normalized_advantages
             clipped_term = tf.clip_by_value(prob_ratio, 1. - self.clip_epsilon, 1. + self.clip_epsilon) * normalized_advantages
-            L_CLIP = tf.reduce_mean(tf.minimum(L_CPI, clipped_term))
+            L_CLIP = tf.reduce_mean(tf.math.minimum(L_CPI, clipped_term))
             loss = -L_CLIP
 
         gradients = tape.gradient(loss, self.actor_trainable_vars)

agents/PolicyGradient.py
@@ -17,7 +17,8 @@ class PolicyGradient:
                  env,
                  learning_rate=1e-3,
                  discount_gamma=0.99,
-                 global_std_for_continuous_policy=False,
+                 global_std_for_gaussian_policy=False,
+                 tanh_transform_gaussian_policy=True,
                  algo_str="PG"):
         self.algo_str = algo_str
         self.env = env  # type: gym.Env
@@ -25,7 +26,8 @@ class PolicyGradient:
         self.discount_gamma = discount_gamma
 
         self.regularizer = None  # tf.keras.regularizers.l2(0.05)
-        self.global_sigma_for_cont_action = global_std_for_continuous_policy
+        self.global_std_for_gaussian_policy = global_std_for_gaussian_policy
+        self.tanh_transform_gaussian_policy = tanh_transform_gaussian_policy
         self.proba_distribution = None  # type: ProbaDistribution
         self.actor_model = None  # type: tf.keras.Model
         self.actor_trainable_vars = None
@@ -40,10 +42,11 @@ class PolicyGradient:
         if isinstance(self.env.action_space, gym.spaces.Discrete):
             self.proba_distribution = Categorical(self.env.action_space)
         elif isinstance(self.env.action_space, gym.spaces.Box):
-            if self.global_sigma_for_cont_action:
-                self.proba_distribution = DiagonalGaussianGlobalStd(self.env.action_space)
+            if self.global_std_for_gaussian_policy:
+                self.proba_distribution = DiagonalGaussianGlobalStd(self.env.action_space,
+                                                                    self.tanh_transform_gaussian_policy)
             else:
-                self.proba_distribution = DiagonalGaussian(self.env.action_space)
+                self.proba_distribution = DiagonalGaussian(self.env.action_space, self.tanh_transform_gaussian_policy)
         self.actor_model = tf.keras.Sequential([
             layers.Dense(64, activation='relu', kernel_regularizer=self.regularizer,
                          input_shape=self.env.observation_space.shape),
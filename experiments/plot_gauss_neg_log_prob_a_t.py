a9bef624761d1ef720a70c3683ef7072b9f04787
kalap <kalapos.andras@gmail.com>
2020-02-19 00:59:46 +0100

    Gaussian neg log prob visualization/plot improved

--- experiments/plot_gauss_neg_log_prob_a_t.py
+++ experiments/plot_gauss_neg_log_prob_a_t.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
-import tensorflow as tf
+# import tensorflow as tf
 import tensorflow_probability as tfp
 
 mean_range = np.arange(-2.5, 2.5, 0.1)
@@ -15,7 +15,7 @@ for i, row in enumerate(mesh_mean):
         dist = tfp.distributions.TransformedDistribution(distribution=dist,
                                                          bijector=tfp.bijectors.Tanh(),
                                                          name='TanhMultivariateNormalDiag')
-        neg_log_prob_a_t = -dist.log_prob([0.9]).numpy()
+        neg_log_prob_a_t = -dist.log_prob([0.]).numpy()
         L[i, j] = neg_log_prob_a_t
 
 L = np.clip(L, -10., 2.)
@@ -27,3 +27,26 @@ plt.xlabel("Mean")
 plt.ylabel("Stdev")
 plt.show()
 
+
+fig = plt.figure()
+lgnd = []
+mean_range = np.arange(0, 1.5, 0.2)
+sigma_range = np.arange(0.1, 2, 0.05)
+for mean in mean_range:
+    lgnd.append("{:0.3f}".format(mean))
+    L = np.zeros_like(sigma_range)
+    for j, sigma in enumerate(sigma_range):
+        dist = tfp.distributions.MultivariateNormalDiag(loc=[mean], scale_diag=[sigma])
+        dist = tfp.distributions.TransformedDistribution(distribution=dist,
+                                                         bijector=tfp.bijectors.Tanh(),
+                                                         name='TanhMultivariateNormalDiag')
+        neg_log_prob_a_t = -dist.log_prob([0.]).numpy()
+        L[j] = neg_log_prob_a_t
+    plt.plot(sigma_range, L)
+
+plt.legend(lgnd)
+plt.ylim([-2., 2.])
+plt.xlabel("Stdev")
+plt.ylabel("-log prob Gaussian(0.)")
+plt.title("-log prob Gaussian(0.) as the function of \n Mean and Std of the probability distribution")
+plt.show()
